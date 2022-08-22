63336c5c712c4f251245c5c00c89c9364ec5923a
McKenzie Macdonald <kenziemacd@icloud.com>
2022-08-22 12:29:54 -0700

    - Add RegisterVC outlets and actions

--- FlashChat/ViewControllers/LoginViewController.swift
+++ FlashChat/ViewControllers/LoginViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+    }
+    
     // MARK: - Navigation
 
 }
