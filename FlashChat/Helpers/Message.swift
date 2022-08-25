0afaef2450863797dbc6d7e87224fa5ee815ea34
McKenzie Macdonald <kenziemacd@icloud.com>
2022-08-25 12:52:12 -0700

    - Display date message sent underneath bubbles
    NOTE: Introduced UI bug where now the bubbles don't fill the screen.

--- FlashChat/Helpers/Message.swift
+++ FlashChat/Helpers/Message.swift
@@ -12,4 +12,10 @@ struct Message {
     let sender: String
     let body: String
     let date: Date
+    
+    var dateString: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy hh:mm"
+        return formatter.string(from: date)
+    }
 }
